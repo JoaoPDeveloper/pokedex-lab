90930a2b5f483737f400e90a516003cb73ece60f
JoaoPDeveloper <joao_rubronegro@hotmail.com>
2022-11-28 23:59:41 -0300

    CSS Edit
    algumas alterações no CSS e javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,22 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
+function imagem(){
+    var img = document.getElementById("img")
+    var imagem = document.getElementById("imagem")
+    if(img.style.display == "none") { //verifica se a imagem está sendo exibida, se não estiver vai executar os comandos abaixo e se tiver vai executar o else
+      img.style.display="block" //exibe a imagem
+      imagem.innerHTML = "CLIQUE DE NOVO" //altera o texto
+    }
+    else { //se a imagem estiver sendo exibida vai executar os comandos abaixo
+      img.style.display="none"; //oculta a imagem
+      imagem.innerHTML = "CLIQUE AQUI" //altera o texto
+    }
+   }
+
+
+
+
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map(convertPokemonToLi).join('')
